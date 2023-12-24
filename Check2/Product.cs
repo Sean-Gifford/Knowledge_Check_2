8a4cf0973dedffeb1cf5d31097d789105c519618
seanmgifford@gmail.com <seanmgifford@gmail.com>
2023-12-24 15:11:11 -0500

    Uploading completed assignment

---
+++ Check2/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check2
+{
+    class Product
+    {
+        public string Name { get; set; }
+
+    }
+
+    class Catfood : Product
+    {
+        public string Type { get; set; }
+    }
+
+}
---
+++ Check2/Program.cs
@@ -0,0 +1,31 @@
+﻿using Check2;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Product>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    Catfood Catfood = new Catfood();
+
+    Console.WriteLine("Enter the value for Name ");
+    Catfood.Name = Console.ReadLine();
+
+    Console.WriteLine("Enter the value for Type ");
+    Catfood.Type = Console.ReadLine();
+
+    recordList.Add(Catfood);
+
+}
+
+Console.WriteLine("");
+Console.WriteLine("The records you entered for each Catfood Name and Catfood Type is:");
+Console.WriteLine("");
+
+foreach (Catfood item in recordList)
+{
+    Console.WriteLine(item.Name);
+    Console.WriteLine(item.Type);
+    Console.WriteLine("");
+}
